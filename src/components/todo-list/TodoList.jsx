5d84343c89bfa3f71fd2d7bb121b0d0814a86b38
Ilhom <ilhom@example.com>
2024-07-04 00:56:54 +0500

    homework

--- src/components/todo-list/TodoList.jsx
+++ src/components/todo-list/TodoList.jsx
@@ -1,37 +1,68 @@
 import "./TodoList.css"
 import {useReducer, useRef} from "react";
+import {FaCheck} from "react-icons/fa";
+import {MdDelete} from "react-icons/md";
+
 
 const reducer = (state, action) => {
-    console.log(action)
+    switch (action.type) {
+        case "TodoList":
+            localStorage.setItem("Todo", JSON.stringify([...state.todo, action.value]))
+            return {
+                ...state,
+                todo: [...state.todo, action.value]
+            }
+        default:
+            return state
+    }
 }
 
 const TodoList = () => {
     const initialValue = {
-        todo: []
+        todo: JSON.parse(localStorage.getItem("Todo")) || [],
+        completed: false
     }
-    const inputValue = useRef("")
+    const inputValue = useRef(null)
 
     const [state, dispatch] = useReducer(reducer, initialValue)
     const formSubmit = (e) => {
         e.preventDefault()
+        const value = inputValue.current.value
+        dispatch({type: "TodoList", value})
 
-        dispatch({type: "Todo", payload: inputValue.current.value})
+        inputValue.current.value = ''
     }
+    const completedTodo = (i) => {
 
-    console.log(state)
+        console.log(i)
+    }
+    const deleteTodo = (i) => {
+        console.log(i)
+    }
 
     return (
         <div className="todo-container">
             <div className="wrapper">
                 <h2>Get Things Done !</h2>
                 <form onSubmit={formSubmit} className="form">
-                    <input ref={inputValue} className="input" type="text" placeholder="What is the task today?" required/>
+                    <input ref={inputValue} className="input" type="text" placeholder="What is the task today?"
+                           required/>
                     <button type="submit" className="btn">Add Task</button>
                 </form>
             </div>
-            <div className="todo-list">
-
-            </div>
+            {
+                state.todo ? state.todo.map((list, index) =>
+                        <div key={index} className="todo-list">
+                            <h2>{list}</h2>
+                            <div className="buttons">
+                                <button onClick={() => completedTodo(index)} className="completed"><FaCheck/></button>
+                                <button onClick={() => deleteTodo(index)} className="delete"><MdDelete/></button>
+                            </div>
+                        </div>
+                    ) :
+                    <>
+                    </>
+            }
         </div>
     )
 }
